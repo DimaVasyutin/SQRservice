0a57b1861f47c6c20a4b85bf3450d775d2b7785f
DimaVasyutin <115107603+DimaVasyutin@users.noreply.github.com>
2022-11-12 13:22:13 +0300

    Without nested loop

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -5,10 +5,8 @@ public class SQRService {
         int count = 0;
         for (int i = 10; i < 100; i++) {
             int sqr = i * i;
-            for (int a = downLimit; a <= upLimit; a++) {
-                if (sqr == a) {
-                    count = count + 1;
-                }
+            if (sqr >= downLimit && sqr <= upLimit) {
+                count = count + 1;
             }
         }
         return count;
